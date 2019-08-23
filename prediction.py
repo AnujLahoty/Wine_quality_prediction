ae72b8b62bf814cb597ea0c233845267c67d65fd
Anuj M Lahoty <48029649+AnujLahoty@users.noreply.github.com>
2019-08-23 20:34:02 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -6,7 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
-import matplotlib.pyplot as plt
 
 # Load the data-set and preprocessing
 df = pd.read_csv('winequality-red.csv')
@@ -52,3 +51,54 @@ rmse_polynomial_regression = np.sqrt(mean_squared_error(y_predicted_poly, y_test
 r_2_score_polynomial_regression = r2_score(y_predicted_poly, y_test)
 ##############################################################################
 
+###############################################################################
+# LOGISTIC REGRESSION (Algorithm number - 3)
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting Logistic Regression to the Training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(multi_class="multinomial",solver="lbfgs", random_state = 0)
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, y_pred))
+
+
+###############################################################################
+# SUPPORT VECTOR REGRESSION (Algorithm number - 4)
+from sklearn.svm import SVC
+classifier = SVC(kernel = "rbf", random_state = 0, decision_function_shape='ovo')
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+print(accuracy_score(y_test, y_pred))
+
+###############################################################################
+# DECISION TREE CLASSIFICATION (Algorithm number - 5)
+from sklearn.ensemble import BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+bag_clf = BaggingClassifier(
+                            DecisionTreeClassifier(), 
+                            n_estimators = 100,
+                            bootstrap = True,
+                            n_jobs = -1,
+                            oob_score = True
+                            )
+
+bag_clf.fit(X_train, y_train)
+y_pred = bag_clf.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, y_pred))
